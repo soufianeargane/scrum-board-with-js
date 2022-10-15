1e84b3f26d81b7c4bb9a421a75ece39084d6a4cc
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2022-10-15 17:27:48 +0100

    starting with create function

---
+++ help.js
@@ -0,0 +1,252 @@
+			// <!-- <div class="row justify-content-center m-0 w-100">
+			// 	<div class=" col-lg-4 col-md-6 col-sm-12  p-1 rounded ">
+			// 		<div class="w-100">
+			// 			<div class="bg-black text-white p-3 rounded-top">
+			// 				<h4 class="">To do (<span id="to-do-tasks-count">5</span>)</h4>
+			// 			</div>
+			// 			<div class="w-100" id="to-do-tasks">
+			// 				<!-- TO DO TASKS HERE -->
+			// 				<button class=" w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-question-circle text-green-500 fs-4"></i> 
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1 ">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-question-circle text-green-500 fs-4"></i> 
+			// 					</div>
+			// 					<div class="d-flex flex-column text-start py-2 ">
+			// 						<div class="fw-bolder h5 mb-1">Consider creating an acceptance criteria list</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#2 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="Descriptive requirements are very helpful when it comes to understanding the context of a problem, yet finally it is good to precisely specify what is expected. Thus the developer will not have to look for the actual requirements in a long, descriptive text but he will be able to easily get to the essence. One might find that sometimes — when acceptance criteria are well defined — there is little or no need for any additional information. Example:
+			// 			a) User navigates to “/accounts” and clicks on red download CSV button
+			// 			b) Popup appears with two buttons: “This year” and “Last year”
+			// 			c) If user clicked on “Last year” download is initiated
+			// 			d) CSV downloaded includes following columns…">Descriptive requirements are very helpful when it comes...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class="rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-question-circle text-green-500 fs-4"></i> 
+			// 					</div>
+			// 					<div class="d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Provide examples, credentials, etc</div>
+			// 						<div class="d-flex flex-column text-start ">
+			// 							<div class="text-gray-600 mb-1">#3 created in 2022-10-08</div>
+			// 							<div class="mb-2 " title="If the expectation is to process or generate some file — attach an example of such a file. If the goal is to integrate what is being developed with some service, ensure your devs have access to this service and its documentation. This list could go on and on — the bottom line is — if there is something that our developer might make use of, try to foresee it and provide them with (access to) it.">If the expectation is to process or generate some file ...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class="rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-question-circle text-green-500 fs-4"></i> 
+			// 					</div>
+			// 					<div class="d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Annotate</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#4 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="The mockup provided can sometimes be confusing for developers. Especially if it contains much more content than the scope of the task described. Drop a couple of arrows, outlines and annotations here and there to emphasize what are the important parts of the mockup from the task requirements perspective.">The mockup provided can sometimes be confusing for deve...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class="rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white rounded-bottom d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-question-circle text-green-500 fs-4 "></i> 
+			// 					</div>
+			// 					<div class="d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Use charts and diagrams</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#5 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="While it is not always necessary, sometimes it might be beneficial to prepare a flowchart, a block diagram or some other kind of concept visualization that will render it easy for the developer to comprehend the task and its scope.">While it is not always necessary, sometimes it might be...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class="rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button>
+
+
+			// 				</button>
+			// 			</div>
+			// 		</div>
+			// 	</div>
+			// 	<div class="col-lg-4 col-md-6 col-sm-12  p-1 rounded ">
+			// 		<div class="w-100">
+			// 			<div class=" bg-black text-white p-3 rounded-top">
+			// 				<h4 class="">In Progress (<span id="in-progress-tasks-count">4</span>)</h4>
+
+			// 			</div>
+			// 			<div class="w-100" id="in-progress-tasks">
+			// 				<!-- IN PROGRESS TASKS HERE -->
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class='bx bx-loader-alt text-green-500 fs-4'></i>
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class='bx bx-loader-alt text-green-500 fs-4'></i> 
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class='bx bx-loader-alt text-green-500 fs-4'></i>
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feagture</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<!-- <i class="bx bx-loader-alt text-green-500 fs-4"></i>  -->
+			// 						<!-- <i class="bx bx-loader-alt"></i> -->
+			// 						<i class='bx bx-loader-alt text-green-500 fs-4'></i>
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 			</div>
+			// 		</div>
+			// 	</div>
+			// 	<div class="col-lg-4 col-md-6 col-sm-12  p-1 rounded ">
+			// 		<div class="w-100">
+			// 			<div class="bg-black text-white p-3 rounded-top">
+			// 				<h4 class="">Done (<span id="done-tasks-count">4</span>)</h4>
+
+			// 			</div>
+			// 			<div class=" w-100" id="done-tasks">
+			// 				<!-- DONE TASKS HERE -->
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-check-circle text-green-500 fs-4"></i> 
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-check-circle text-green-500 fs-4"></i> 
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-check-circle text-green-500 fs-4"></i> 
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 				<button class="w-100 border-0 mb-1 bg-white d-flex">
+			// 					<div class="p-2">
+			// 						<i class="bi bi-check-circle text-green-500 fs-4"></i> 
+			// 					</div>
+			// 					<div class=" d-flex flex-column text-start py-2">
+			// 						<div class="fw-bolder h5 mb-1">Keep all the updated requirements in one place</div>
+			// 						<div class="d-flex flex-column text-start">
+			// 							<div class="text-gray-600 mb-1">#1 created in 2022-10-08</div>
+			// 							<div class="mb-2" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">There is hardly anything more frustrating than having t...</div>
+			// 						</div>
+			// 						<div class="">
+			// 							<span class="rounded px-2 py-1 text-white bg-cyan-600">High</span>
+			// 							<span class=" rounded px-2 py-1 bg-gray-400">Feature</span>
+			// 						</div>
+			// 					</div>
+			// 				</button>
+			// 			</div>
+			// 		</div>
+			// 	</div>
+			// </div> -->
---
+++ main.js
@@ -0,0 +1,6 @@
+function create (){
+    let myTitle = document.getElementById("formTitle").value;
+    let myFeature = document.getElementById("feature");
+    let myBug = document.getElementById("bug");
+    if(myFeature.checked)
+}
